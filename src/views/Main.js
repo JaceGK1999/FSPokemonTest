3fd3e901d6013c0669596876d95f48a0b6cd256a
Jacegk1999 <Jacegk1999@gmail.com>
2022-04-26 12:15:43 -0700

    set constants and state/ setup main in app

---
+++ src/views/Main.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+import { fetchPokemon, fetchTypes } from '../services/FetchPokemon';
+import { TypeSearch } from '../components/Search/TypeSearch';
+import { fetchFilteredPokemon } from '../services/FetchFilteredPokemon';
+
+export default function Main() {
+  return (
+    <div>Main</div>
+  )
+}
